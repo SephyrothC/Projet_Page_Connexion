6c3943a91b347b842c95bae9a9bc3d3a15dadd5e
Adrian Stephan <97838183+SephyrothC@users.noreply.github.com>
2024-03-06 11:46:37 +0100

    Update portfolio.php

--- templates/portfolio.php
+++ templates/portfolio.php
@@ -1,108 +1,116 @@
-<!doctype html>
-<html class="no-js" lang="" xmlns="http://www.w3.org/1999/html">
+<?php session_start();
+  if (isset($_SESSION['username'])): ?>
+  <!doctype html>
+  <html class="no-js" lang="" xmlns="http://www.w3.org/1999/html">
 
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Portfolio Adrian Stephan</title>
-  <link rel="stylesheet" href="./assets/css/portfolio.css">
-  <meta name="description" content="">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Portfolio Adrian Stephan</title>
+    <link rel="stylesheet" href="./assets/css/portfolio.css">
+    <meta name="description" content="">
 
-  <meta name="robot" content="index, nofollow">
-  <meta property="og:title" content="">
-  <meta property="og:type" content="">
-  <meta property="og:url" content="">
-  <meta property="og:image" content="">
+    <meta name="robot" content="index, nofollow">
+    <meta property="og:title" content="">
+    <meta property="og:type" content="">
+    <meta property="og:url" content="">
+    <meta property="og:image" content="">
 
-  <link rel="icon" href="./favicon.ico" sizes="any">
-  <link rel="icon" href="./icon.svg" type="image/svg+xml">
-  <link rel="apple-touch-icon" href="icon.png">
+    <link rel="icon" href="./favicon.ico" sizes="any">
+    <link rel="icon" href="./icon.svg" type="image/svg+xml">
+    <link rel="apple-touch-icon" href="icon.png">
 
-  <link rel="manifest" href="site.webmanifest">
-  <meta name="theme-color" content="#fafafa">
-  <script type="text/javascript" src="./assets/js/const.js"></script>
-  <script type="text/javascript" src="./assets/js/jquery.js"></script>
-  <title>typing animation</title>
-  <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.11/lib/typed.min.js"></script>
-  <script type="text/javascript" src="./assets/js/app.js"></script>
+    <link rel="manifest" href="site.webmanifest">
+    <meta name="theme-color" content="#fafafa">
+    <script type="text/javascript" src="./assets/js/const.js"></script>
+    <script type="text/javascript" src="./assets/js/jquery.js"></script>
+    <title>typing animation</title>
+    <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.11/lib/typed.min.js"></script>
+    <script type="text/javascript" src="./assets/js/app.js"></script>
 
-</head>
+  </head>
 
-<body>
+  <body>
 
-  <header>
-    <div id = "bg_fade">
-      <img src ="./assets/img/logo.png" id="logo">
-      <div id= "txt_P1">
-        <div id="name">
-        Adrian Stephan
+    <header>
+      <div id = "bg_fade">
+        <img src ="./assets/img/logo.png" id="logo">
+        <div id= "txt_P1">
+          <div id="name">
+          Adrian Stephan
+          </div>
+          
+          <p>
+            Portfolio
+          </p>
         </div>
+      </div>
+
+    </header>
+
+
+    <main>
+      <section id="AboutMe">
+        <div id="photoAboutme">
+          <img src="./assets/img/photoAboutme.jpg" alt="Photo of me">
+        </div>
+        <div id="Me">
+          <h2>
+            About me
+          </h2>
+          <p>
+            Je suis une personne comme les autres, avec mes qualités et mes défauts. J'aime découvrir de nouvelles choses et
+            apprendre de nouvelles compétences. J'ai aussi mes passions et mes loisirs, qui me permettent de m'évader du quotidien.
+          </p>
+
+          <a id="Bouton" href="#Level_Grade">
+              Learn more ↓ 
+          </a>
+        </div>
+
         
+      </section >
+
+      <section id="Level_Grade">
+        <div id ="Grade_Title">
+          <h2>
+            Mes Skills
+          </h2>
+        </div>
+        <div id="Grade">
+
+        
+        </div>
+      </section>
+      <section id="Project">
+        
+      </section>
+    </main>
+
+
+    <footer>
+      <div id="copyright">
         <p>
-          Portfolio
+          &copy; 2024 Adrian Stephan
         </p>
       </div>
-    </div>
-
-  </header>
-
-
-  <main>
-    <section id="AboutMe">
-      <div id="photoAboutme">
-        <img src="./assets/img/photoAboutme.jpg" alt="Photo of me">
-      </div>
-      <div id="Me">
-        <h2>
-          About me
-        </h2>
-        <p>
-          Je suis une personne comme les autres, avec mes qualités et mes défauts. J'aime découvrir de nouvelles choses et
-          apprendre de nouvelles compétences. J'ai aussi mes passions et mes loisirs, qui me permettent de m'évader du quotidien.
-        </p>
-
-        <a id="Bouton" href="#Level_Grade">
-            Learn more ↓ 
+      <div id="social">
+        <a href="https://github.com/SephyrothC" target="_blank">
+          <img src="./assets/img/github_logo.png" alt="Github">
+        </a>
+        <a href="https://www.linkedin.com/in/adrian-st%C3%A9phan-029980258/" target="_blank">
+          <img src="./assets/img/linkedin_logo.png" alt="LinkedIn">
         </a>
       </div>
 
-      
-    </section >
+    </footer>
 
-    <section id="Level_Grade">
-      <div id ="Grade_Title">
-        <h2>
-          Mes Skills
-        </h2>
-      </div>
-      <div id="Grade">
+  </body>
 
-       
-      </div>
-    </section>
-    <section id="Project">
-      
-    </section>
-  </main>
-
-
-  <footer>
-    <div id="copyright">
-      <p>
-        &copy; 2024 Adrian Stephan
-      </p>
-    </div>
-    <div id="social">
-      <a href="https://github.com/SephyrothC" target="_blank">
-        <img src="./assets/img/github_logo.png" alt="Github">
-      </a>
-      <a href="https://www.linkedin.com/in/adrian-st%C3%A9phan-029980258/" target="_blank">
-        <img src="./assets/img/linkedin_logo.png" alt="LinkedIn">
-      </a>
-    </div>
-
-  </footer>
-
-</body>
-
-</html>
+  </html>
+  <?php else: ?>
+    <!-- error 404 -->
+    <?php 
+    include "../404.html" 
+    ?>
+<?php endif; ?>
